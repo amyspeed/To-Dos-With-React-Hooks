c20f01e492b542a3c354789f019cf1b9a8109689
Amy Speed <amy.speed.henley@gmail.com>
2020-05-28 22:18:36 -0500

    styles

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <div className="app">
-      <div className="todos">
+      <div className="todo-list">
         { todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo}/>
         ))}
